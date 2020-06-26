e5fac9b3e7621e6af642e3517223445e42a8ff73
HubUser99 <cliff.goga@gmail.com>
2020-06-26 23:24:54 +0300

    Updated project to render a rotating triangle :)

--- tools/build.bat
+++ tools/build.bat
@@ -2,5 +2,5 @@
 
 mkdir build
 pushd build
-cl -EHsc -Zi /MD /I ..\deps\GLFW\include ..\src\program.cpp ..\deps\GLFW\lib\glfw3.lib opengl32.lib User32.lib Gdi32.lib Shell32.lib
+cl -EHsc -Zi /MD /I ..\deps\include ..\src\program.cpp ..\src\glad.c ..\deps\lib\GLFW\glfw3.lib opengl32.lib User32.lib Gdi32.lib Shell32.lib
 popd
